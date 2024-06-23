8fd2c249249a32e1aa34c1b36ad8139e640940c2
JorgePeniaranda <peniarandajorge@gmail.com>
2024-06-23 16:40:31 -0300

    feat: initialize swagger

---
+++ src/constants/app.ts
@@ -0,0 +1,7 @@
+export const APP_INFO = {
+  DEFAULT_PORT: 3000,
+  VERSION: '1.0.0',
+  NAME: 'Keepify API',
+  DESCRIPTION: 'Secure Keep Notes API',
+  SWAGGER_PATH: 'api',
+} as const;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { APP_INFO } from './constants/app';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* add documentation */
+  const config = new DocumentBuilder()
+    .setTitle(APP_INFO.NAME)
+    .setDescription(APP_INFO.DESCRIPTION)
+    .setVersion(APP_INFO.VERSION)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(APP_INFO.SWAGGER_PATH, app, document);
+
   await app.listen(3000);
 }
 bootstrap();
